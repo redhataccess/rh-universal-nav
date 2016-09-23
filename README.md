0eca1d4d8be3e04025a51136ddd83ec95a344aa8
Kyle Buchanan <kylebuch8@gmail.com>
2016-09-22 21:56:23 -0400

    created the web component

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Red Hat Universal Nav
+
+A universal nav web component.
+
+## Getting Started
+```
+bower install
+```
+
+## About
+
+This uses the [webcomponentsjs polyfill](http://webcomponents.org/) to create a cross-browser universal navigation web component. It uses the web component v0 spec and will need to be updated to the v1 spec when it's more widely supported.
--- rh-universal-nav.js
+++ rh-universal-nav.js
@@ -1,7 +1,29 @@
-class RhUniversalNav extends HTMLElement {
-  createdCallback() {
-    console.log('working');
-  }
-};
+(function () {
+  class RhUniversalNav extends HTMLElement {
+    createdCallback() {
+      var shadowRoot = this.createShadowRoot();
+      shadowRoot.innerHTML = `
+        <content select="[rhwc-aux-nav]"></content>
+        <form class="rhwc-search">
+          <div data-name="web-icon-search" class="icons__item">
+            <i class="web-icon-search"></i> web-icon-search
+            <span class="icons__tag">tags:magnifying, glass, look</span>
+          </div>
+        </form>
+        <content select="[rhwc-search-tray]"></content>
+        <div data-name="web-icon-user" class="icons__item">
+          <i class="web-icon-user"></i> web-icon-user
+          <span class="icons__tag">tags:social, user, profile, person</span>
+        </div>
+        <content select="[rhwc-login-tray]"></content>
+        <div data-name="web-icon-globe" class="icons__item">
+          <i class="web-icon-globe"></i> web-icon-globe
+          <span class="icons__tag">tags: world, language, country</span>
+        </div>
+        <content name="[rhwc-lang-tray]"></content>
+      `;
+    }
+  };
 
-document.registerElement('rh-universal-nav', RhUniversalNav);
+  document.registerElement('rh-universal-nav', RhUniversalNav);
+}());
